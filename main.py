e00061b807543d2f41184f76a41fe035e9705ede
Aaron <terran8633@gmail.com>
2025-04-29 03:13:47 +0900

    Add btn2(clear) and handler function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, 
-                             QMessageBox, QPlainTextEdit)
+                             QMessageBox, QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon # icon을 추가하기 위한 라이브러리
 
 class Calculator(QWidget):
@@ -12,13 +12,21 @@ class Calculator(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
         
-        
         self.btn1 = QPushButton('Message', self) # Add Button
         self.btn1.clicked.connect(self.activateMessage) # if click button, connect handler function
         
+        self.btn2 = QPushButton('Clear', self)
+        self.btn2.clicked.connect(self.clearMessage)
+        
+        hbox = QHBoxLayout() # Create horizontal layout widget
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+        
         vbox = QVBoxLayout() # Create vertical layout widget
         vbox.addWidget(self.te1) # TextEdit of location
-        vbox.addWidget(self.btn1) # Button of location
+        vbox.addLayout(hbox)
+        # vbox.addWidget(self.btn1) # Button of location
         vbox.addStretch(1) # space
         
         self.setLayout(vbox) # space - button - space layout setting
@@ -32,6 +40,8 @@ class Calculator(QWidget):
         # QMessageBox.information(self, 'information', 'Button clicked !')
         self.te1.appendPlainText("Button Clicked !")
         
+    def clearMessage(self):
+        self.te1.clear()
         
 if __name__=="__main__":
     app = QApplication(sys.argv)

c2ecae30c7e7d17e544864693977acb3d7dd8db5
Aaron <terran8633@gmail.com>
2025-05-07 22:16:17 +0900

    Refactor main.py to be implemented in ui.py and ctrl.py

--- main.py
+++ main.py
@@ -1,49 +1,15 @@
-import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, 
-                             QMessageBox, QPlainTextEdit, QHBoxLayout)
-from PyQt5.QtGui import QIcon # icon을 추가하기 위한 라이브러리
+# ch 5.2.1 main.py
 
-class Calculator(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        
-    def initUI(self):
-        self.te1 = QPlainTextEdit()
-        self.te1.setReadOnly(True)
-        
-        self.btn1 = QPushButton('Message', self) # Add Button
-        self.btn1.clicked.connect(self.activateMessage) # if click button, connect handler function
-        
-        self.btn2 = QPushButton('Clear', self)
-        self.btn2.clicked.connect(self.clearMessage)
-        
-        hbox = QHBoxLayout() # Create horizontal layout widget
-        hbox.addStretch(1)
-        hbox.addWidget(self.btn1)
-        hbox.addWidget(self.btn2)
-        
-        vbox = QVBoxLayout() # Create vertical layout widget
-        vbox.addWidget(self.te1) # TextEdit of location
-        vbox.addLayout(hbox)
-        # vbox.addWidget(self.btn1) # Button of location
-        vbox.addStretch(1) # space
-        
-        self.setLayout(vbox) # space - button - space layout setting
-        
-        self.setWindowTitle('Calculator')
-        self.setWindowIcon(QIcon('icon.png'))
-        self.resize(256, 256)
-        self.show()
-        
-    def activateMessage(self): # if click button, execute this function
-        # QMessageBox.information(self, 'information', 'Button clicked !')
-        self.te1.appendPlainText("Button Clicked !")
-        
-    def clearMessage(self):
-        self.te1.clear()
-        
-if __name__=="__main__":
+import sys
+from ui import View # ui.py의 View 클래스 추가
+from ctrl import Control # ctrl.py의 Control 클래스 추가
+from PyQt5.QtWidgets import QApplication
+
+def main(): # 프로그램 실행(Application) 관련 내용 함수화
     app = QApplication(sys.argv)
-    view = Calculator()
+    view = View()
+    Control(view=view)
     sys.exit(app.exec_())
+
+if __name__=="__main__":
+    main()
